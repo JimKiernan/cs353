508076b4608c08c3a3653844b507a05635c337da
JimKiernan <48436779+JimKiernan@users.noreply.github.com>
2019-03-11 19:11:34 +0000

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello 3 World!'))
+app.get('/', (req, res) => res.send('Failte Aise!'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
