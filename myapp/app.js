a0880c108d35839f1dbe481b74ecfe31372ee2fb
Elizabeth Garvey <ezgarvey@gmail.com>
2019-05-06 14:15:25 +0100

    removed myapp

--- myapp/app.js
+++
@@ -1,4 +0,0 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Failte Aise!'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
